f003a8bbfedf8e4644dc4553300da14de7768580
Kevin Huang <huangyf9883@gmail.com>
2026-01-15 17:06:34 +0800

    refactor(utils): 将 URL 映射提取到单独的文件中

--- src/components/ChampionStatistics.vue
+++ src/components/ChampionStatistics.vue
@@ -74,24 +74,8 @@
 import { ref, onMounted } from 'vue'
 import { fetchMarioWorkerYaml, extractSeasonData } from '../utils/yamlLoader'
 import { getEditionNumber } from '../utils/editionHelper'
+import { uploadUrlMap as urlMap } from '../utils/urlMap'
 
-// 网盘/上传系统URL映射
-const urlMap: Record<string, string> = {
-  '2025': 'https://mwcup2025.marioforever.net',
-  '2024': 'https://mwcup2024.marioforever.net',
-  '2023': 'https://mwcup2023.marioforever.net',
-  '2022': 'https://mwcup2022.marioforever.net',
-  '2021': 'http://2021mwcup.yspean.com/',
-  '2020': 'http://mwcup2020.yspean.com/',
-  '2019': 'http://mwcup2019.yspean.com/',
-  '2018': 'http://mwcup2018.yspean.com/',
-  '2017': 'http://2017mwcup.yspean.com/',
-  '2016': 'http://mwcup2016.yspean.com/',
-  '2015': 'http://mwcup2015.yspean.com/',
-  '2014': 'http://mwcup3--2014.yspean.com/',
-  '2013': 'http://2013mwcup.yspean.com/',
-  '2012': 'http://mwcup.yspean.com/'
-}
 import { loadRoundScoreData } from '../utils/scoreCalculator'
 import { Decimal } from 'decimal.js'
 
---
+++ src/utils/urlMap.ts
@@ -0,0 +1,20 @@
+export const uploadUrlMap: Record<string, string> = {
+  '2025': 'https://mwcup2025.marioforever.net',
+  '2024': 'https://mwcup2024.marioforever.net',
+  '2023': 'https://mwcup2023.marioforever.net',
+  '2022': 'https://mwcup2022.marioforever.net',
+  '2021': 'http://2021mwcup.yspean.com/',
+  '2020': 'http://mwcup2020.yspean.com/',
+  '2019': 'http://mwcup2019.yspean.com/',
+  '2018': 'http://mwcup2018.yspean.com/',
+  '2017': 'http://2017mwcup.yspean.com/',
+  '2016': 'http://mwcup2016.yspean.com/',
+  '2015': 'http://mwcup2015.yspean.com/',
+  '2014': 'http://mwcup3--2014.yspean.com/',
+  '2013': 'http://2013mwcup.yspean.com/',
+  '2012': 'http://mwcup.yspean.com/'
+}
+
+export function getUploadUrl(year: string): string | undefined {
+  return uploadUrlMap[year]
+}
